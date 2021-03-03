308db42d5d03f6a04a3217e3ade17e4ac18ae42c
serveyk0 <ssimpl8ss@gmail.com>
2021-03-03 17:24:38 +0200

    init

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,15 @@ import { Films } from '../pages/Films';
 import history from '../functions/history';
 import { AllPagesData } from '../pages/AllPagesData';
 const Routers = () => {
+    const href = window.location.href.split("/")[3] === 'swapi' ? '/swapi' : '';
+    console.log(href);
     return (
         <Router history={history}>
-            <NavLink className="margin-0-auto text-center title" to='/swapi/films'>star wars</NavLink>
+            <NavLink className="margin-0-auto text-center title" to={href + '/films'}>star wars</NavLink>
             <Switch>
-                <Route path='/:path/:id' component={AllPagesData} exact />
-                <Route path='/films' component={Films} exact />
-                <Route path="*" render={() => <Redirect to="/swapi/films" />} />
+                <Route path={href + '/:path/:id'} component={AllPagesData} exact />
+                <Route path={href + '/films'} component={Films} exact />
+                <Route path="*" render={() => <Redirect to={href + "/films"} />} />
             </Switch>
         </Router>
     )
